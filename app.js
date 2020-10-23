c49cb486d761d2b9ee0f82c64b03f7b7b8e79319
viveloper <ucsik1234@gmail.com>
2020-10-23 14:05:09 +0900

    Working with Express

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
+const path = require('path');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(3000, () => {
-  console.log('running server on port 3000');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
